e57ae563e84b627ce1484bde33fc5f8bd18b2fae
Alexponomarev7 <Alex.ponomarev7@ya.ru>
2016-05-02 23:59:12 +0300

    changed

--- field.py
+++ field.py
@@ -1,29 +1,31 @@
 import tkinter
 import threading
 
-def load(number):
+def load(number=1):
+    global fields
+    
     fin = open('level'+str(number)+'.txt','r')
-    field = fin.readlines()
-    for i in range(len(field)):
-        field[i] = field[i].strip()
+    fields = fin.readlines()
+    for i in range(len(fields)):
+        fields[i] = fields[i].strip()
     render = threading.Thread(target = field)
     render.start()
 
 def field():
-    global field
+    global fields
     game_window = tkinter.Tk()
-    game_wiindow.title('GAME')
+    game_window.title('GAME')
     game_window.geometry('600x600+300+300')
     
     game_field = tkinter.Canvas(game_window, width = 600, height = 600)
     game_field.place(relx=0, rely=0)
     
-    root.protocol('WM_DELETE_WINDOW', game_window.destroy())
+    game_window.protocol('WM_DELETE_WINDOW', game_window.destroy)
     
     const_len = 20
     for i in range(10, 601, const_len):
         for j in range(10, 601, const_len):
-            if field[i][j] == '#':
+            if fields[i][j] == '#':
                 w.create_rectangle(j - const_len // 2, i + const_len // 2, j + const_len // 2, i - const_len // 2, fill = 'black')
             else:
                 w.create_rectangle(j - const_len // 2, i + const_len // 2, j + const_len // 2, i - const_len // 2, fill = 'blue')
--- game.py
+++ game.py
@@ -4,8 +4,9 @@ import field
 
 #1368
 def window_deleted():
-    print('Окно закрыто, ёпть')
+    print('Р›РѕРѕРѕР»')
     root.destroy()
+
 
 root = tkinter.Tk()
 root.title(u"LabirintOfPonomar")
@@ -14,10 +15,10 @@ root.protocol('WM_DELETE_WINDOW', window_deleted)
 root.resizable(False, False)
 frame1 = tkinter.Frame(root, width = 580, height = 460, bg = 'black', bd = 5)
 frame1.place(anchor = 'center', relx = 0.5, rely = 0.5, relwidth = 0.99, relheight = 0.99)
-but1 = tkinter.Button(root, text = 'Начать игру', width = 25, height = 5, bg = 'white', fg = 'black', font = 'arial 14', command=field.load(1)) # вызов функции
+but1 = tkinter.Button(root, text = 'РќРѕРІР°СЏ РёРіСЂР°', width = 25, height = 5, bg = 'white', fg = 'black', font = 'arial 14', command=field.load) 
 but1.place(anchor = 'center', relx= 0.5, rely=0.3, relwidth = 0.25, relheight = 0.05)
 levels = tkinter.Listbox(root, selectmode=tkinter.SINGLE)
-lev = [u"Уровень 1", u"Уровень 2"]
+lev = [u"РџРµСЂРІС‹Р№ 1", u"Р’С‚РѕСЂРѕР№ 2"]
 for i in lev:
     levels.insert(tkinter.END, i)
 levels.place(anchor = 'center', relx=0.5,  rely = 0.4, relwidth = 0.1, relheight = 0.1)
